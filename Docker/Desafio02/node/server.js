997a78c6c974b485f3d11e064aedcb9990c79cd7
aldeni <aldeni.filho@gmail.com>
2021-08-13 15:02:11 -0300

    Full Cycle - Trilha DevOps - Modulo 1 -Docker / Desafio 02

---
+++ Docker/Desafio02/node/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+const mysql = require('mysql')
+const config = {
+        host: 'db',
+        user: 'root',
+        password: 'root',
+        database: 'dockerdesafio02'
+};
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    const cnn = mysql.createConnection(config)
+    const sqlinsert = `INSERT INTO people (name) VALUES ('Jose');`
+
+    cnn.query(sqlinsert)
+
+    const sqlselect = `SELECT * FROM people;`
+
+    cnn.query(sqlselect, (err, data, fields) => {
+        if (err) throw err
+            res.render('../views/home', {nomes: data})
+    })
+    cnn.end()
+})
+
+app.listen(port,() => {
+    console.log('Running on port ' + port)
+})
